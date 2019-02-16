698e3e8c2d0dde68fcb63c6c934d22a7463c5489
ivan <c.ivan.felix.r@gmail.com>
2019-02-15 18:32:49 -0600

    routes

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,52 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
-/*  This is a sample API route. */
+const players = [
+   { firstName: 'eli', lastNmae: "manning", position: "qb", age:23, team:"nyg" },
+   { firstName: 'tom', lastNmae: "brady", position: "qb", age:32, team:"ny" },
+   { firstName: 'dido', lastNmae: "watt", position: "bc", age:23, team:"ny" },
+]
 
-router.get('/:resource', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		query: req.query // from the url query string
-	})
+const teams = [
+   { name:"giants", city:"new york", conference:"nfc" },
+   { name:"patriots", city:"new england", conference:"afc" },
+   { name:"texans", city:"houston", conference:"nfc" },
+]
+
+const db = {
+   players,
+   teams
+}
+
+router.get("/:resource", (req, res) => {
+   const resource = req.params.resource
+   let data = db[resource]
+   let confirmation = "success"
+
+
+   if(data == null){
+      confirmation = "failed"
+      data = "No avaliable data for this resource"
+   }
+
+   res.json({
+      confirmation,
+      data
+   })
 })
 
-router.get('/:resource/:id', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		id: req.params.id,
-		query: req.query // from the url query string
-	})
-})
+//router.get( '/players', (req, res) =>{
+   //res.json({
+      //confirmation: 'success',
+      //data: players
+   //})
+//})
 
-
+//router.get( '/teams', (req, res) =>{
+   //res.json({
+      //confirmation: 'success',
+      //data: teams
+   //})
+//})
 
 module.exports = router
