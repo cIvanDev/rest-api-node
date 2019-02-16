31c2db6bf1c30ad57da0f21cda388a14c09b594e
ivan <c.ivan.felix.r@gmail.com>
2019-02-16 13:03:25 -0600

    controllers and models

---
+++ controllers/TeamController.js
@@ -0,0 +1,15 @@
+const Team = require('../models/Team')
+
+module.exports = {
+   get: params => {
+      return new Promise( (resolve, reject) => {
+         Team.find(params)
+            .then(data => {
+               resolve(data)
+            })
+            .catch(err => {
+               reject(err)
+            })
+      })
+   }
+}
--- routes/api.js
+++ routes/api.js
@@ -3,52 +3,33 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
-const players = [
-   { firstName: 'eli', lastNmae: "manning", position: "qb", age:23, team:"nyg" },
-   { firstName: 'tom', lastNmae: "brady", position: "qb", age:32, team:"ny" },
-   { firstName: 'dido', lastNmae: "watt", position: "bc", age:23, team:"ny" },
-]
+const controllers = require('../controllers')
 
-const teams = [
-   { name:"giants", city:"new york", conference:"nfc" },
-   { name:"patriots", city:"new england", conference:"afc" },
-   { name:"texans", city:"houston", conference:"nfc" },
-]
-
-const db = {
-   players,
-   teams
-}
-
-router.get("/:resource", (req, res) => {
+router.get('/:resource', (req, res) => {
    const resource = req.params.resource
-   let data = db[resource]
-   let confirmation = "success"
+   const controller = controllers[resource]
 
-
-   if(data == null){
-      confirmation = "failed"
-      data = "No avaliable data for this resource"
+   if(controller == null){
+      res.json({
+         confirmation: 'fail',
+         message: 'Invalid Resource',
+      })
+      return
    }
 
-   res.json({
-      confirmation,
-      data
-   })
+   controller.get()
+      .then(data => {
+         res.json({
+            confirmation: 'success',
+            data
+         })
+      })
+      .catch(err => {
+         res.json({
+            confirmation: 'fail',
+            message: err.message,
+         })
+      })
 })
-
-//router.get( '/players', (req, res) =>{
-   //res.json({
-      //confirmation: 'success',
-      //data: players
-   //})
-//})
-
-//router.get( '/teams', (req, res) =>{
-   //res.json({
-      //confirmation: 'success',
-      //data: teams
-   //})
-//})
 
 module.exports = router
